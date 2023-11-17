1fbeaa9a4d1f4012ca8189946aa2d4d2f34d4562
Christian Sumido <gcbsumid@gmail.com>
2023-11-16 19:20:55 -0800

    Updated Event Manager and Cleaned up Demo Helika Asset (#1)
    In this PR:
    - Cleaned up Demo Helika Event to allow setting some variables in the
    editor
    - Cleaned up EventManager

---
+++ Assets/HelikaSdk/DemoHelikaAsset.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using UnityEngine;
+using Helika;
+using Newtonsoft.Json.Linq;
+
+namespace Rawrshak
+{
+    public class DemoHelikaAsset : MonoBehaviour
+    {
+        public string ApiKey;
+        public string GameId;
+        public string BaseUrl = Helika.HelikaBaseURL.Localhost;
+        public bool SendingEvents = false;
+        private Helika.EventManager eventManager;
+
+        void Start()
+        {
+            eventManager = Helika.EventManager.Instance;
+            eventManager.Init(ApiKey, GameId, BaseUrl, SendingEvents);
+        }
+
+
+        async void Update()
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                JObject startEvent = new JObject(
+                    new JProperty("game_id", "Test Project"),
+                    new JProperty("event_type", "Start Event"),
+                    new JProperty("event", new JObject(
+                        new JProperty("user_id", 10),
+                        new JProperty("name", "John Doe"),
+                        new JProperty("email", "john@doe.com")
+                    ))
+                );
+                JObject endEvent = new JObject(
+                    new JProperty("game_id", "Test Project"),
+                    new JProperty("event_type", "End Event"),
+                    new JProperty("event", new JObject(
+                        new JProperty("user_id", 10)
+                    ))
+                );
+                JObject[] testEvents = new JObject[] { startEvent, endEvent };
+                await eventManager.SendEvent(testEvents);
+            }
+        }
+    }
+}
