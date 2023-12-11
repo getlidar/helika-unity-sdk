feea6bafaf4c9160d6d98a350b659c6c9cb08512
Christian Sumido <gcbsumid@gmail.com>
2023-12-11 14:16:29 -0800

    Added the new SendEvent() endpoints

--- Assets/Helika/Source/EventManager.cs
+++ Assets/Helika/Source/EventManager.cs
@@ -33,7 +33,7 @@ namespace Helika
 
         protected bool _enabled = false;
 
-        public async Task Init(string apiKey, string gameId, HelikaEnvironment env, bool enabled = false)
+        public async void Init(string apiKey, string gameId, HelikaEnvironment env, bool enabled = false)
         {
             if (_isInitialized)
             {
@@ -67,27 +67,144 @@ namespace Helika
             KochavaTracker.Instance.RegisterIosAppGuid(_kochavaApiKey);
             KochavaTracker.Instance.Start();
 
-            await CreateSession();
-
-            // Send an event to store the Kochava device id
-            KochavaTracker.Instance.GetDeviceId(async (deviceId) =>
+            if (KochavaTracker.Instance != null)
             {
-                this._deviceId = deviceId;
+                // Send an event to store the Kochava device id
+                KochavaTracker.Instance.GetDeviceId((deviceId) =>
+                {
+                    this._deviceId = deviceId;
 
-                JObject deviceIdEvent = new JObject(
-                    new JProperty("game_id", "HELIKA_SDK"),
-                    new JProperty("event_type", "GET_DEVICE_ID"),
-                    new JProperty("event", new JObject(
-                        new JProperty("kochava_device_id", deviceId)
-                    ))
-                );
-
-                // Asynchronous send event
-                await SendEvent(new JObject[] { deviceIdEvent });
-            });
+#pragma warning disable CS4014
+                    // Fire and forget generate a 'Create Session'
+                    CreateSession();
+#pragma warning restore CS4014
+                });
+            }
+            else
+            {
+                // In case the KochavaTracker fails to initialized
+                await CreateSession();
+            }
         }
 
-        public async Task<string> SendEvent(JObject[] helikaEvents)
+        public async Task<string> SendEvent(string eventName, Dictionary<string, object> eventProps)
+        {
+            if (!_isInitialized)
+            {
+                throw new Exception("Event Manager is not yet initialized");
+            }
+
+            Dictionary<string, object> helikaEvent = new Dictionary<string, object>();
+
+            // Add game_id only if the event doesn't already have it
+            helikaEvent["game_id"] = _gameId;
+
+            // Convert to ISO 8601 format string using "o" specifier
+            helikaEvent["created_at"] = DateTime.UtcNow.ToString("o");
+
+            // Set event_type if it doesn't exist
+            helikaEvent["event_type"] = eventName;
+
+            eventProps["sessionID"] = _sessionID;
+            if (!string.IsNullOrWhiteSpace(_playerID))
+            {
+                eventProps["player_id"] = _playerID;
+            }
+
+            helikaEvent["event"] = eventProps;
+
+
+            Dictionary<string, object> finalEvent = new Dictionary<string, object>();
+            finalEvent["id"] = _sessionID;
+            finalEvent["events"] = new Dictionary<string, object>[] { helikaEvent };
+
+            JObject serializedEvt = JObject.FromObject(finalEvent);
+            return await PostAsync("/game/game-event", serializedEvt.ToString());
+        }
+
+        public async Task<string> SendEvents(string eventName, Dictionary<string, object>[] eventsProps)
+        {
+            if (!_isInitialized)
+            {
+                throw new Exception("Event Manager is not yet initialized");
+            }
+            // Add helika-specific data to the events
+            List<Dictionary<string, object>> events = new List<Dictionary<string, object>> { };
+            foreach (Dictionary<string, object> eventProps in eventsProps)
+            {
+                Dictionary<string, object> helikaEvent = new Dictionary<string, object>()
+                {
+                    // Add game_id only if the event doesn't already have it
+                    {"game_id", _gameId},
+                    // Convert to ISO 8601 format string using "o" specifier
+                    {"created_at", DateTime.UtcNow.ToString("o")},
+                    // Set event_type
+                    {"event_type", eventName},
+                };
+
+                eventProps["sessionID"] = _sessionID;
+                if (!string.IsNullOrWhiteSpace(_playerID))
+                {
+                    eventProps["player_id"] = _playerID;
+                }
+
+                helikaEvent["event"] = eventProps;
+
+                events.Add(helikaEvent);
+            }
+
+
+            Dictionary<string, object> finalEvent = new Dictionary<string, object>();
+            finalEvent["id"] = _sessionID;
+            finalEvent["events"] = events.ToArray();
+
+            JObject serializedEvt = JObject.FromObject(finalEvent);
+            return await PostAsync("/game/game-event", serializedEvt.ToString());
+        }
+
+        public async Task<string> SendEvent(JObject eventProps)
+        {
+            if (!_isInitialized)
+            {
+                throw new Exception("Event Manager is not yet initialized");
+            }
+
+            // Add game_id only if the event doesn't already have it
+            AddIfNull(eventProps, "game_id", _gameId);
+
+            // Convert to ISO 8601 format string using "o" specifier
+            AddOrReplace(eventProps, "created_at", DateTime.UtcNow.ToString("o"));
+
+            if (!eventProps.ContainsKey("event_type") || string.IsNullOrWhiteSpace(eventProps.GetValue("event_type").ToString()))
+            {
+                throw new ArgumentException("Invalid Event: Missing 'event_type' field");
+            }
+
+            if (!eventProps.ContainsKey("event"))
+            {
+                eventProps.Add(new JProperty("event", new JObject()));
+            }
+
+            if (eventProps.GetValue("event").GetType() != typeof(Newtonsoft.Json.Linq.JObject))
+            {
+                throw new ArgumentException("Invalid Event: 'event' field must be of type [Newtonsoft.Json.Linq.JObject]");
+            }
+
+            JObject internalEvent = (JObject)eventProps.GetValue("event");
+            AddOrReplace(internalEvent, "sessionID", _sessionID);
+            if (!string.IsNullOrWhiteSpace(_playerID))
+            {
+                AddOrReplace(internalEvent, "player_id", _playerID);
+            }
+
+            JObject newEvent = new JObject(
+                new JProperty("id", _sessionID),
+                new JProperty("events", new JArray() { eventProps })
+            );
+            return await PostAsync("/game/game-event", newEvent.ToString());
+        }
+
+        public async Task<string> SendEvents(JObject[] eventsProps)
         {
             if (!_isInitialized)
             {
@@ -96,50 +213,43 @@ namespace Helika
 
             // Add helika-specific data to the events
             JArray jarrayObj = new JArray();
-            foreach (JObject helikaEvent in helikaEvents)
+            foreach (JObject eventProp in eventsProps)
             {
                 // Add game_id only if the event doesn't already have it
-                AddIfNull(helikaEvent, "game_id", _gameId);
+                AddIfNull(eventProp, "game_id", _gameId);
 
                 // Convert to ISO 8601 format string using "o" specifier
-                AddOrReplace(helikaEvent, "created_at", DateTime.UtcNow.ToString("o"));
+                AddOrReplace(eventProp, "created_at", DateTime.UtcNow.ToString("o"));
 
-                if (!helikaEvent.ContainsKey("event_type") || string.IsNullOrWhiteSpace(helikaEvent.GetValue("event_type").ToString()))
+                if (!eventProp.ContainsKey("event_type") || string.IsNullOrWhiteSpace(eventProp.GetValue("event_type").ToString()))
                 {
                     throw new ArgumentException("Invalid Event: Missing 'event_type' field");
                 }
 
-                if (!helikaEvent.ContainsKey("event"))
+                if (!eventProp.ContainsKey("event"))
                 {
-                    helikaEvent.Add(new JProperty("event", new JObject()));
+                    eventProp.Add(new JProperty("event", new JObject()));
                 }
 
-                if (helikaEvent.GetValue("event").GetType() != typeof(Newtonsoft.Json.Linq.JObject))
+                if (eventProp.GetValue("event").GetType() != typeof(Newtonsoft.Json.Linq.JObject))
                 {
                     throw new ArgumentException("Invalid Event: 'event' field must be of type [Newtonsoft.Json.Linq.JObject]");
                 }
 
-                JObject internalEvent = (JObject)helikaEvent.GetValue("event");
+                JObject internalEvent = (JObject)eventProp.GetValue("event");
                 AddOrReplace(internalEvent, "sessionID", _sessionID);
 
                 if (!string.IsNullOrWhiteSpace(_playerID))
                 {
                     AddOrReplace(internalEvent, "player_id", _playerID);
                 }
-                jarrayObj.Add(helikaEvent);
+                jarrayObj.Add(eventProp);
             }
 
             JObject newEvent = new JObject(
                 new JProperty("id", _sessionID),
                 new JProperty("events", jarrayObj)
             );
-
-            if (!_enabled)
-            {
-                var message = "Event sent: " + newEvent.ToString();
-                Debug.Log(message);
-                return message;
-            }
             return await PostAsync("/game/game-event", newEvent.ToString());
         }
 
@@ -161,19 +271,37 @@ namespace Helika
         private async Task<string> CreateSession()
         {
             JObject createSessionEvent = new JObject(
-                new JProperty("game_id", "HELIKA_SDK"),
+                new JProperty("game_id", _gameId),
                 new JProperty("event_type", "SESSION_CREATED"),
+                new JProperty("created_at", DateTime.UtcNow.ToString("o")),
                 new JProperty("event", new JObject(
+                    new JProperty("sessionID", _sessionID),
+                    new JProperty("player_id", _playerID),
                     new JProperty("sdk_name", SdkName),
                     new JProperty("sdk_version", SdkVersion),
-                    new JProperty("sdk_class", SdkClass)
+                    new JProperty("sdk_class", SdkClass),
+                    new JProperty("kochava_device_id", _deviceId)
                 ))
             );
-            return await SendEvent(new JObject[] { createSessionEvent });
+
+            JObject evt = new JObject(
+                new JProperty("id", _sessionID),
+                new JProperty("events", new JArray() { createSessionEvent })
+            );
+
+            // Asynchronous send event
+            return await PostAsync("/game/game-event", evt.ToString());
         }
 
         private async Task<string> PostAsync(string url, string data)
         {
+            if (!_enabled)
+            {
+                var message = "Event sent: " + data;
+                Debug.Log(message);
+                return message;
+            }
+
             using (UnityWebRequest request = new UnityWebRequest(_baseUrl + url, "POST"))
             {
                 // Set the request method and content type
