6bb24d4061bf264ed252dfe421ec754a8d72a0b5
Christian Sumido <gcbsumid@gmail.com>
2024-10-31 14:01:16 -0700

    Updated HelikaAssetScript

--- Assets/Helika/HelikaAssetScript.cs
+++ Assets/Helika/HelikaAssetScript.cs
@@ -22,62 +22,73 @@ namespace Helika
         void Start()
         {
             eventManager = EventManager.Instance;
+            eventManager.SetUserDetails(
+                new JObject(
+                    new JProperty("user_id", 10),
+                    new JProperty("email", "test@gmail.com"),
+                    new JProperty("wallet", "0x8540507642419A0A8Af94Ba127F175dA090B58B0")
+                )
+            );
+            eventManager.SetAppDetails(
+                new JObject(
+                    new JProperty("platform_id", "Android"),
+                    new JProperty("client_app_version", "0.0.1"),
+                    new JProperty("server_app_version", null),
+                    new JProperty("store_id", "EpicGames"),
+                    new JProperty("source_id", null)
+                )
+            );
             eventManager.Init(apiKey, gameId, helikaEnv, telemetry, printEventsToConsole);
-            eventManager.SetPlayerID(playerId);
         }
 
         // void Update()
         // {
         //     if (Input.GetKeyDown("space"))
         //     {
-        //         // This is an example of sending a single event
-        //         Dictionary<string, object> dictionary = new Dictionary<string, object>
-        //         {
-        //             { "user_id", 10 },
-        //             { "wallet_address", "0x8540507642419A0A8Af94Ba127F175dA090B58B0" }
-        //         };
-        //         eventManager.SendEvent("connect_wallet", dictionary);
-
-        //         // This is an example of sending multiples of a single event
-        //         Dictionary<string, object> evt1 = new Dictionary<string, object>
-        //         {
-        //             { "user_id", 10 },
-        //             { "source", "win" },
-        //             { "points", 15 }
-        //         };
-        //         Dictionary<string, object> evt2 = new Dictionary<string, object>
-        //         {
-        //             { "user_id", 10 },
-        //             { "source", "top_3_player" },
-        //             { "points", 5 }
-        //         };
-        //         eventManager.SendEvents("add_points", new Dictionary<string, object>[] { evt1, evt2 });
-
-        //         // If you'd like to send a whole game events and overwrite automatically added properties like 'game_id', 
-        //         // you can use the EventManager.SendCustomEvent() and EventManager.SendCustomEvents().
-        //         // !! Note: Most of the time, you should only use EventManager.SendEvent() and EventManager.SendEvents().
-        //         JObject startEvent = new JObject(
-        //             new JProperty("event_type", "start_event"),
+        //         // This is an example of sending a single user event
+        //         JObject playerKilledEvent = new JObject(
+        //             new JProperty("event_type", "player_killed"),
         //             new JProperty("event", new JObject(
-        //                 new JProperty("user_id", 10),
-        //                 new JProperty("name", "John Doe"),
-        //                 new JProperty("email", "john@doe.com")
-        //             ))
+        //                     new JProperty("user_id", 10),
+        //                     new JProperty("damage_amount", 40),
+        //                     new JProperty("bullets_fired", 15),
+        //                     new JProperty("map", "arctic")
+        //                 )
+        //             )
         //         );
-        //         // This event has no 'event' data. We automatically generate it.
-        //         JObject middleEvent = new JObject(
-        //             new JProperty("event_type", "no_event")
-        //         );
-        //         // This event overwrites the `game_id` field
-        //         JObject endEvent = new JObject(
-        //             new JProperty("game_id", "Override Project"),
-        //             new JProperty("event_type", "end_event"),
+        //         eventManager.SendUserEvent(playerKilledEvent);
+
+        //         // This is an example of sending multiple events at once
+        //         JObject evt1 = new JObject(
+        //             new JProperty("event_type", "bomb_planted"),
         //             new JProperty("event", new JObject(
-        //                 new JProperty("user_id", 10)
-        //             ))
+        //                     new JProperty("map", "arctic"),
+        //                     new JProperty("team", "counter-terrorists")
+        //                 )
+        //             )
         //         );
-        //         JObject[] uniqueEvents = new JObject[] { startEvent, middleEvent, endEvent };
-        //         eventManager.SendCustomEvents(uniqueEvents);
+
+        //         JObject evt2 = new JObject(
+        //             new JProperty("event_type", "bomb_diffused"),
+        //             new JProperty("event", new JObject(
+        //                     new JProperty("map", "arctic"),
+        //                     new JProperty("team", "counter-terrorists"),
+        //                     new JProperty("duration", 10210.121)
+        //                 )
+        //             )
+        //         );
+        //         eventManager.SendUserEvents(new JArray() { evt1, evt2 });
+
+        //         // This is an example of a non-user event. For Non-user events, we don't automatically append user information
+        //         JObject winEvent = new JObject(
+        //             new JProperty("event_type", "game_finished"),
+        //             new JProperty("event", new JObject(
+        //                     new JProperty("winner", "counter-terrorists"),
+        //                     new JProperty("map", "arctic")
+        //                 )
+        //             )
+        //         );
+        //         eventManager.SendEvent(winEvent);
         //     }
         // }
     }
