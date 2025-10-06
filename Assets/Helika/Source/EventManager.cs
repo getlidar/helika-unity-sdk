fd3babc616e1f82cbf8045079250fa8f8bc03adf
Christian Sumido <gcbsumid@gmail.com>
2025-10-06 14:12:55 -0700

    fixed url localhost

--- Assets/Helika/Source/EventManager.cs
+++ Assets/Helika/Source/EventManager.cs
@@ -435,7 +435,7 @@ namespace Helika
                     return "https://events.analytics.helika.io";
                 case HelikaEnvironment.Localhost:
                 default:
-                    return "http://localhost:8181";
+                    return "http://localhost:8182";
             }
         }
 
