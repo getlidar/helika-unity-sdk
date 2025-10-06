9d177ee31bd414e45bcf3735fefb40f63bd5c681
Christian Sumido <gcbsumid@gmail.com>
2025-10-06 14:11:37 -0700

    Fixed trailing slash

--- Assets/Helika/Source/EventManager.cs
+++ Assets/Helika/Source/EventManager.cs
@@ -432,7 +432,7 @@ namespace Helika
                 case HelikaEnvironment.Production:
                     return "https://events.analytics.helika.io";
                 case HelikaEnvironment.Develop:
-                    return "https://events.analytics.helika.io/";
+                    return "https://events.analytics.helika.io";
                 case HelikaEnvironment.Localhost:
                 default:
                     return "http://localhost:8181/v1";
